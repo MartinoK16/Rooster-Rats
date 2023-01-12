7df99a163c4bb1e5cbaf29751243ba0d753b7695
Melanie Messih <melaniemessih@hotmail.com>
2023-01-12 13:14:16 +0100

    started working on a student rooster

--- classes.py
+++ classes.py
@@ -202,15 +202,17 @@ nr_days = 5
 
 # ------------------------------------------------------------------------------------------------------------------------------------------
 class Room():
-    def __init__(self, nr_timeslots, nr_days, room, evening, room_capacity, day, timeslot):
+    def __init__(self, nr_timeslots, nr_days, room, evening, room_capacity, day, timeslot, course):
         self.nr_timeslots = nr_timeslots # Standard; without evening timeslot
         self.nr_days = nr_days
         self.room = room
         self.evening = evening # Boolean
-
         self.capacity = room_capacity
+
+        # Wat moet je hiermee?
         self.day = day
         self.timeslot = timeslot
+        self.course = course
 
         # Create first version of rooster and availability
         if self.evening:
@@ -222,63 +224,27 @@ class Room():
 
     def check_availability(self):
         """
-        Accepts a 3D array of shape nr_rooms x nr_days x nr_timeslots and checks the
-        availability for a given room. The function returns a boolean 3D array of
-        shape nr_days x nr_timeslots which shows if the timeslot is occupied (True)
-        or not (False).
+        Accepts a 3D array of shape nr_rooms x nr_days x nr_timeslots and checks
+        the availability for a given room. The function returns a boolean 3D
+        array of shape nr_days x nr_timeslots which shows if the timeslot is
+        occupied (True) or not (False).
         """
-        # Check availability for the room; switch True and False (occupied = 1; not occupied = 0)
+        # Check availability for the room; switch True and False (occupied = 1;
+        # not occupied = 0)
         self.availability = np.invert(self.rooster[self.room] == self.availability)
 
     def remove_course(self, timeslot):
         self.rooster[day, timeslot] = 0
+        # Ook availability updaten naar 0?
 
-    def add_course(self, timeslot, )
+    def add_course(self, timeslot):
+        self.rooster[day, timeslot] = self.course
+        # Ook availability updaten naar 1?
 
+output_df = pd.read_csv('LecturesLesroosters/test.csv')
 
-class Experiment():
-    def __init__(self, poging):
-        self.poging = poging
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def add_rooms(self, number_of_rabbits):
-        """
-        This function accepts a number of rabbits and fills a list of
-        number_of_rabbits rabbits with random locations and angles.
-        """
-        for i in range(number_of_rabbits):
-            # Create a random location and angle for the rabbit
-            self.create_random_pos_angle()
-            # Add the random location and angle of the rabbit to creatures list
-            self.creatures.append(Rabbit(self.pos_x, self.pos_y, self.angle))
-# Create Room object
-
-
-
-
-# # Check availability for each room; switch True and False (occupied = 1; not occupied = 0)
-# for room in range(nr_rooms):
-#     test = check_availability(self, room_rooster, availability, room)
-#     test2 = np.invert(test)
-#     print(test2 * 5)
-
-
-
-        # Give the lecture its name, type, lecture code and student numbers
-        d = {'H': 1, 'W': 2, 'P': 3}
-        self.name = lecture_name
-        self.type = lecture_type
-        self.studs = lecture_studs
-        self.code = int(f'{class_nr + 11}{d[lecture_type[0]]}{lecture_type[1]}')
+# Loop over the different rooms
+for student in output_df['student'].unique():
+    # Get expected people of the rooms
+    expected = output_df[output_df['student'] == student]
+    print(expected)
