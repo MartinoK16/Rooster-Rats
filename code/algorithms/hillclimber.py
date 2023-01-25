ef3e830bc44875462b466dd0dac2475f4725e046
Melanie Messih <melaniemessih@hotmail.com>
2023-01-25 20:40:42 +0100

    Meer functiebeschrijvingen in Rooster class; details veranderd

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -9,6 +9,7 @@ class Hillclimber():
         self.students = rooster.students
         self.activities = rooster.activities
 
+
     def hc_activities(self):
         '''
         Does one loop over all the activities in a random order and finds the
@@ -16,54 +17,42 @@ class Hillclimber():
         (activities or empty slots).
         '''
         # Loop over all the activities randomly
-        for nr3, lecture1 in enumerate(random.sample(self.activities, len(self.activities))):
-            # Get the room and slot where this lecture is place now
-            room1 = lecture1.room
-            slot1 = lecture1.slot
+        for nr3, activity1 in enumerate(random.sample(self.activities, len(self.activities))):
+            # Get the room and slot where this activity is place now
+            room1 = activity1.room
+            slot1 = activity1.slot
             # Make a dictionary to track the malus points for each slot
             tries = {}
             # Loop over all the slots in each room
             for nr2, room2 in enumerate(self.rooms):
                 for slot2 in np.ndindex(room2.rooster.shape):
-                    # Get which lecture is placed in this room and slot
-                    lecture2 = room2.rooster[slot2]
+                    # Get which activity is placed in this room and slot
+                    activity2 = room2.rooster[slot2]
                     # Swap these 2 activities, get the new malus and put it in the dict and swap them back
-                    self.swap_course(room1, lecture1, slot1, room2, lecture2, slot2)
+                    self.swap_course(room1, activity1, slot1, room2, activity2, slot2)
                     tries[nr2, slot2] = sum(Evaluation(self).malus_count())
-                    self.swap_course(room1, lecture2, slot1, room2, lecture1, slot2)
+                    self.swap_course(room1, activity2, slot1, room2, activity1, slot2)
 
             # Randomly get one of the slots with the least malus points
             slot = random.choice([k for k, v in tries.items() if v==min(tries.values())])
 
-            # Get which room and lecture need to be switched and swap them
+            # Get which room and activity need to be switched and swap them
             room2 = self.rooms[slot[0]]
-            lecture2 = room2.rooster[slot[1]]
-            self.swap_course(room1, lecture1, slot1, room2, lecture2, slot[1])
+            activity2 = room2.rooster[slot[1]]
+            self.swap_course(room1, activity1, slot1, room2, activity2, slot[1])
 
             # Get the updated malus count and print useful info
             malus = Evaluation(self).malus_count()
-            print(lecture1.code, malus, sum(malus), nr3)
+            print(activity1.code, malus, sum(malus), nr3)
 
-    def create_stud_set(self, stud_list, add_zero=True):
-        stud_set = set(stud_list)
-        if add_zero:
-            stud_set.add(0)
-        # print(stud_set)
-        return stud_set
-
-    def try_swap(self, student, group, new_group, new_group_nr, tries_dict, add_zero=True):
-        for other_student in self.create_stud_set(new_group.studs, add_zero):
-            self.swap_student(student, group, group.slot, other_student, new_group, new_group.slot)
-            tries_dict[(new_group_nr, other_student)] = sum(Evaluation(self).malus_count())
-            self.swap_student(other_student, group, group.slot, student, new_group, new_group.slot)
-            return tries_dict
 
     def hc_students(self, tut_or_prac):
-        """
-        Accepts a string, 'T' for tutorial or 'P' for practical, and moves or
-        swaps students to another tutorial group or practical group, based on a
-        decreasing number of malus points.
-        """
+        '''
+        Accepts a string, 'T' for tutorial or 'P' for practical, and loops over
+        all the courses in a random order. Finds the best fit for each student
+        in tutorial or practical groups by swapping with all other possibilities
+        (students or empty spots).
+        '''
         # Loop over all courses randomly
         for nr, course in enumerate(random.sample(self.courses, len(self.courses))): # ENUMERATE WEGHALEN
             # Loop over all tutorial / practical groups
@@ -79,7 +68,7 @@ class Hillclimber():
                     for new_group_nr in range(len(getattr(course, tut_or_prac))):
                         new_group = getattr(course, tut_or_prac)[new_group_nr]
 
-                        # Check if swapping and moving the student are possible
+                        # Check if capacity is not already reached before swapping or moving the student
                         if new_group_nr != group_nr and new_group.max_studs > new_group.size:
                             self.try_swap(student, group, new_group, new_group_nr, tries)
                         elif new_group_nr != group_nr and new_group.max_studs == new_group.size:
@@ -88,64 +77,92 @@ class Hillclimber():
                     # Randomly get one of the swaps with the least malus points
                     best_swap = random.choice([k for k, v in tries.items() if v==min(tries.values())])
 
-                    # Select correct group and student
+                    # Select correct group and student and perform best swap
                     best_swap_group = getattr(course, tut_or_prac)[best_swap[0]]
                     best_swap_stud = best_swap[1]
-
-                    # Perform this best swap if it is not equal to the current situation
                     self.swap_student(student, group, group.slot, best_swap_stud, best_swap_group, best_swap_group.slot)
 
+                # Get the updated malus count and print useful info
                 malus = Evaluation(self).malus_count()
                 print((malus, nr, tut_or_prac), sum(malus))
 
-    def swap_course(self, room1, lec1, slot1, room2, lec2, slot2):
+
+    def swap_course(self, room1, act1, slot1, room2, act2, slot2):
         '''
-        Swaps 2 activities from room and slot and updates the corresponding student roosters
+        Accepts two rooms, their activities and slots. Swaps two activities from
+        room and slot and updates the corresponding student roosters.
         '''
         # Swap the 2 activities in the room roosters
-        room1.rooster[slot1] = lec2
-        room2.rooster[slot2] = lec1
+        room1.rooster[slot1] = act2
+        room2.rooster[slot2] = act1
 
-        # Check if it is a lecture and not an empty slot
-        if lec1 != 0:
-            # Update the lecture attributes
-            lec1.room = room2
-            lec1.slot = slot2
-            # Update all the roosters for the students with this lecture
-            for stud in lec1.studs:
-                stud.swap_lecture(lec1, slot1, lec1, slot2)
+        # Check if it is an activity and not an empty slot
+        if act1 != 0:
+            # Update the activity attributes
+            act1.room = room2
+            act1.slot = slot2
+            # Update all the roosters for the students with this activity
+            for stud in act1.studs:
+                stud.swap_activity(act1, slot1, act1, slot2)
 
-        # Check if it is a lecture and not an empty slot
-        if lec2 != 0:
-            # Update the lecture attributes
-            lec2.room = room1
-            lec2.slot = slot1
-            # Update all the roosters for the students with this lecture
-            for stud in lec2.studs:
-                stud.swap_lecture(lec2, slot2, lec2, slot1)
+        # Check if it is an activity and not an empty slot
+        if act2 != 0:
+            # Update the activity attributes
+            act2.room = room1
+            act2.slot = slot1
+            # Update all the roosters for the students with this activity
+            for stud in act2.studs:
+                stud.swap_activity(act2, slot2, act2, slot1)
 
         # Update the malus counts for both rooms
         room1.update_malus()
         room2.update_malus()
 
-    def swap_student(self, student1, lec1, slot1, student2, lec2, slot2):
+
+    def swap_student(self, student1, act1, slot1, student2, act2, slot2):
         '''
-        Removes a student from a lecture and adds it to another one.
-        Also updates the student rooster and malus points
+        Accepts two students, their current activities and slots. If student is
+        not 0, student is removed from their activity and added to the other
+        activity. Also updates the student roosters and the malus points.
         '''
         if student1 != 0:
-            lec1.studs.remove(student1)
-            lec1.size -= 1
-            lec2.studs.append(student1)
-            lec2.size += 1
-            student1.swap_lecture(lec1, slot1, lec2, slot2)
+            act1.studs.remove(student1)
+            act1.size -= 1
+            act2.studs.append(student1)
+            act2.size += 1
+            student1.swap_activity(act1, slot1, act2, slot2)
 
         if student2 != 0:
-            lec2.studs.remove(student2)
-            lec2.size -= 1
-            lec1.studs.append(student2)
-            lec1.size += 1
-            student2.swap_lecture(lec2, slot2, lec1, slot1)
+            act2.studs.remove(student2)
+            act2.size -= 1
+            act1.studs.append(student2)
+            act1.size += 1
+            student2.swap_activity(act2, slot2, act1, slot1)
 
-        lec1.room.update_malus()
-        lec2.room.update_malus()
+        act1.room.update_malus()
+        act2.room.update_malus()
+
+
+    def create_stud_set(self, stud_list, add_zero=True):
+        '''
+        Accepts a list and an optional boolean argument. Transforms list into
+        set, either with or without adding 0 (True vs. False, respectively).
+        '''
+        stud_set = set(stud_list)
+        if add_zero:
+            stud_set.add(0)
+        return stud_set
+
+
+    def try_swap(self, student, group, new_group, new_group_nr, tries_dict, add_zero=True):
+        '''
+        Accepts student to be swapped, current group of the student, new group,
+        number of the new group, a tries dictionary and an optional boolean
+        argument. Transforms student list of new group into set, swaps students,
+        gets the new malus, puts it in the dictionary and swaps them back.
+        '''
+        for other_student in self.create_stud_set(new_group.studs, add_zero):
+            self.swap_student(student, group, group.slot, other_student, new_group, new_group.slot)
+            tries_dict[(new_group_nr, other_student)] = sum(Evaluation(self).malus_count())
+            self.swap_student(other_student, group, group.slot, student, new_group, new_group.slot)
+            return tries_dict
