f72055cc2a81afc70fe25330acdcb427d2e3d1d0
kmvangerwen <122450544+kmvangerwen@users.noreply.github.com>
2023-01-31 14:53:11 +0100

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -6,15 +6,24 @@ import numpy as np
 import time
 import argparse
 import yaml
+import pdfschedule
 
-from student_rooster import *
+from classes.rooster import Rooster
+
+from student_rooster import rooster_per_student
 from classes.rooster import *
 from algorithms.evaluation import *
 from algorithms.hillclimber import *
 from algorithms.initialize import *
 from algorithms.tabu_search import *
+from experiments import *
 
-def main(version, bool):
+print('Welkom,')
+Print('We are Rooster-Rats and we tried to solve the Scheduling problem.')
+Print('We did the following experiments:')
+print('\t -')
+
+def main(version, csv):
     courses_df = pd.read_csv('../data/vakken.csv')
     student_df = pd.read_csv('../data/studenten_en_vakken2.csv')
     rooms_df = pd.read_csv('../data/zalen.csv')
@@ -36,8 +45,15 @@ def main(version, bool):
         print(f'Malus division version 2: {malus2}')
         print(f'Total malus: {sum(malus2)}')
         # Create output csv
-        if bool == 1:
+        if csv == 1:
             my_rooster2.make_csv('../data/rooster_v2.csv')
+
+        if plot == 1:
+            make_plot(nr_runs=20, 'random', 'random')
+
+        if hist == 1:
+            make_histogram(1000, 'random')
+
 
     if version == 3:
         my_rooster3 = Rooster(courses_df, student_df, rooms_df, evenings)
@@ -47,8 +63,15 @@ def main(version, bool):
         print(f'Malus division version 3: {malus3}')
         print(f'Total malus: {sum(malus3)}')
         # Create output csv
-        if bool == 1:
+        if csv == 1:
             my_rooster3.make_csv('../data/rooster_v3.csv')
+
+        if hc_plot == 1:
+            make_plot(nr_runs=20, 'large to small')
+
+        if hist == 1:
+            make_histogram(1000, 'large to small')
+
 
     if version == 4:
         my_rooster4 = Rooster(courses_df, student_df, rooms_df, evenings)
@@ -59,11 +82,24 @@ def main(version, bool):
         print(f'Total malus: {sum(malus4)}')
 
         # Create output csv
-        if bool == 1:
+        if csv == 1:
             my_rooster4.make_csv('../data/rooster_v4.csv')
             output_df_v4 = pd.read_csv('../data/rooster_v4.csv')
             rooster_p_student= Evaluation(my_rooster4).rooster_per_student(output_df_v4)
             print(f'Rooster of last student ({rooster_p_student[0]}):\n{rooster_p_student[1]}')
+
+        if plot == 1:
+            make_plot(nr_runs=20, 'greedy')
+            make_histogram(1000, 'greedy')
+
+    if version == 5:
+        SA_experiment()
+
+
+    if version == 6:
+        make_plot(nr_runs=20, type_rooster='greedy')
+        make_histogram(1000, 'greedy')
+
 
 
 
@@ -84,10 +120,10 @@ if __name__ == '__main__':
 
 
     parser.add_argument('-v', '--version', type=int, default=5, help='desired rooster version: 2 - 5')
-    parser.add_argument('-b', '--bool', type=int, default=0, help='make output false[0] or true[1]')
+    parser.add_argument('-csv', '--csv', type=int, default=0, help='make csv false[0] or true[1]')
 
     # Read arguments from command line
     args = parser.parse_args()
 
     # Run main with provide arguments
-    main(args.version, args.bool)
+    main(args.version, args.csv)
