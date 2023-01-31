a0f8ea0e529e5e5736cfd1c7a40a1129b75a6d62
kmvangerwen <122450544+kmvangerwen@users.noreply.github.com>
2023-01-31 15:49:43 +0100

    Add files via upload
    new main

--- main.py
+++ main.py
@@ -16,59 +16,122 @@ from algorithms.evaluation import *
 from algorithms.hillclimber import *
 from algorithms.initialize import *
 from algorithms.tabu_search import *
+from experiments import *
 
-def main(version, bool):
+print('Welkom,')
+Print('We are Rooster-Rats and we tried to solve the Scheduling problem.')
+Print('We did the following experiments:')
+print('\t -')
+
+def main(algorithm, csv):
     courses_df = pd.read_csv('../data/vakken.csv')
     student_df = pd.read_csv('../data/studenten_en_vakken2.csv')
     rooms_df = pd.read_csv('../data/zalen.csv')
 
     # Rooms with evening timeslot
     evenings = {'C0.110'}
+    my_rooster = Rooster(courses_df, student_df, rooms_df, evenings)
+    my_rooster = Initialize(my_rooster)
 
     print('Malus points for version 2, 3 or 4:')
 
-    if version == 1:
-        print('This version was just a test fase.')
-        print('The versions 2, 3, 4 have implemented algorithms')
-
-    if version == 2:
-        my_rooster2 = Rooster(courses_df, student_df, rooms_df, evenings)
-        my_rooster2 = Initialize(my_rooster2)
-        my_rooster2.make_rooster_random(4, 5, 7)
-        malus2 = Evaluation(my_rooster2).malus_count()
-        print(f'Malus division version 2: {malus2}')
-        print(f'Total malus: {sum(malus2)}')
+    if algorithm == 'random':
+        my_rooster.make_rooster_random(4, 5, 7)
+        malus = Evaluation(my_rooster).malus_count()
+        print(f'Malus division version 2: {malus}')
+        print(f'Total malus for: {sum(malus)}')
         # Create output csv
-        if bool == 1:
-            my_rooster2.make_csv('../data/rooster_v2.csv')
 
-    if version == 3:
-        my_rooster3 = Rooster(courses_df, student_df, rooms_df, evenings)
-        my_rooster3 = Initialize(my_rooster3)
-        my_rooster3.make_rooster_largetosmall()
-        malus3 = Evaluation(my_rooster3).malus_count()
-        print(f'Malus division version 3: {malus3}')
-        print(f'Total malus: {sum(malus3)}')
-        # Create output csv
-        if bool == 1:
-            my_rooster3.make_csv('../data/rooster_v3.csv')
+        if csv == 1:
+            my_rooster2.make_csv('../data/rooster_random.csv')
 
-    if version == 4:
-        my_rooster4 = Rooster(courses_df, student_df, rooms_df, evenings)
-        my_rooster4 = Initialize(my_rooster4)
-        my_rooster4.make_rooster_greedy()
-        malus4 = Evaluation(my_rooster4).malus_count()
-        print(f'Malus division version 3: {malus4}')
-        print(f'Total malus: {sum(malus4)}')
+        if plot == 1:
+            make_histogram(1000, 'random')
+
+    if algorithm == 'greedy':
+        my_rooster.make_rooster_greedy()
+        malus = Evaluation(my_rooster).malus_count()
+        print(f'Malus division version 3: {malus}')
+        print(f'Total malus: {sum(malus)}')
 
         # Create output csv
-        if bool == 1:
+        if csv == 1:
             my_rooster4.make_csv('../data/rooster_v4.csv')
-            output_df_v4 = pd.read_csv('../data/rooster_v4.csv')
+            output_df_v4 = pd.read_csv('../data/rooster_greedy.csv')
             rooster_p_student= Evaluation(my_rooster4).rooster_per_student(output_df_v4)
             print(f'Rooster of last student ({rooster_p_student[0]}):\n{rooster_p_student[1]}')
 
+        if plot == 1:
+            make_histogram(1000, 'greedy')
 
+    if algorithm == 'random hillclimber':
+        my_rooster.make_rooster_random(4, 5, 7)
+        my_rooster = Hillclimber(my_rooster)
+        my_rooster.hc_activities()
+        print(Evaluation(my_rooster).malus_count())
+
+        # Create output csv
+        if csv == 1:
+            my_rooster.make_csv('../data/rooster_random_hillclimber.csv')
+
+        if plot == 1:
+            make_plot(nr_runs=10, 'random', 'hc_activities')
+
+    if algorithm == 'greedy hillclimber':
+        my_rooster.make_rooster_greedy()
+        my_rooster = Hillclimber(my_rooster)
+        my_rooster.hc_activities()
+        print(Evaluation(my_rooster).malus_count())
+
+        # Create output csv
+        if csv == 1:
+            my_rooster.make_csv('../data/rooster_greedy_hillclimber.csv')
+
+        if plot == 1:
+            make_plot(nr_runs=10, 'greedy', 'hc_activities')
+
+    if algorithm == 'random hillclimber with students':
+        my_rooster.make_rooster_random(4, 5, 7)
+        my_rooster = Hillclimber(my_rooster)
+        my_rooster.hc_activities()
+        my_rooster.hc_students('T')
+        my_rooster.hc_students('P')
+        print(Evaluation(my_rooster).malus_count())
+
+        # Create output csv
+        if csv == 1:
+            my_rooster.make_csv('../data/rooster_random_hillclimber_stud.csv')
+
+        if plot == 1:
+            make_plot(nr_runs=10, 'random', 'hc_activities_and_students')
+
+    if algorithm == 'greedy hillclimber with students':
+        my_rooster.make_rooster_greedy()
+        my_rooster = Hillclimber(my_rooster)
+        my_rooster.hc_activities()
+        my_rooster.hc_students('T')
+        my_rooster.hc_students('P')
+        print(Evaluation(my_rooster).malus_count())
+
+        # Create output csv
+        if csv == 1:
+            my_rooster.make_csv('../data/rooster_greedy_hillclimber_stud.csv')
+
+        if plot == 1:
+            make_plot(nr_runs=10, 'greedy', 'hc_activities_and_students')
+
+    if algorithm == 'greedy simulated annealing':
+        my_rooster.make_rooster_greedy()
+        Simulated_Annealing(my_rooster, 50, 50000).run()
+        print(Evaluation(my_rooster).malus_count())
+
+        # Create output csv
+        if csv == 1:
+            my_rooster.make_csv('../data/rooster_simulated_annealing.csv')
+
+        if plot == 1:
+            SA_experiment()
+            
 
 """
 Create rooster visualisation of all 7 rooms.
@@ -86,11 +149,11 @@ if __name__ == '__main__':
     # Adding arguments
 
 
-    parser.add_argument('-v', '--version', type=int, default=5, help='desired rooster version: 2 - 5')
-    parser.add_argument('-b', '--bool', type=int, default=0, help='make output false[0] or true[1]')
+    parser.add_argument('-algo', '--algorithm', type=int, default=5, help='desired algorithm')
+    parser.add_argument('-csv', '--csv', type=int, default=0, help='make csv false[0] or true[1]')
 
     # Read arguments from command line
     args = parser.parse_args()
 
     # Run main with provide arguments
-    main(args.version, args.bool)
+    main(args.version, args.csv)
